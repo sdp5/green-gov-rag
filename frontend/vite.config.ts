becac6fe6b67a09196de264929535626ab4a842f
Sundeep Anand <sundeep.sdp5@gmail.com>
2025-10-16 09:18:19 +1030

    add Vite HMR config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,4 +10,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+    },
+  },
 })
